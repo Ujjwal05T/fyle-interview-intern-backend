38ae4bcf89a621e0a1c8646cd00ce62ef4db4f9d
Ujjwal <utamrakar3@gmail.com>
2024-11-04 22:18:30 +0530

    Changes in core apis,models and corrected test cases also added some new

---
+++ core/apis/teachers/schema.py
@@ -0,0 +1,14 @@
+from marshmallow import EXCLUDE, post_load
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
+from core.models.teachers import Teacher
+
+
+class TeacherSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Teacher
+        unknown = EXCLUDE
+
+    id = auto_field(required=True, allow_none=True)
+    user_id = auto_field(dump_only=True)
+    created_at = auto_field(dump_only=True)
+    updated_at = auto_field(dump_only=True)
--- core/server.py
+++ core/server.py
@@ -2,6 +2,8 @@ from flask import jsonify
 from marshmallow.exceptions import ValidationError
 from core import app
 from core.apis.assignments import student_assignments_resources, teacher_assignments_resources
+from core.apis.assignments.principal import principal_assignments_resources
+from core.apis.teachers import principal_teachers_resources
 from core.libs import helpers
 from core.libs.exceptions import FyleError
 from werkzeug.exceptions import HTTPException
@@ -10,6 +12,8 @@ from sqlalchemy.exc import IntegrityError
 
 app.register_blueprint(student_assignments_resources, url_prefix='/student')
 app.register_blueprint(teacher_assignments_resources, url_prefix='/teacher')
+app.register_blueprint(principal_assignments_resources, url_prefix='/principal')
+app.register_blueprint(principal_teachers_resources, url_prefix='/principal')
 
 
 @app.route('/')
@@ -40,5 +44,5 @@ def handle_error(err):
         return jsonify(
             error=err.__class__.__name__, message=str(err)
         ), err.code
-
+    
     raise err
